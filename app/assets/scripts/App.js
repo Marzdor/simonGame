96ba48af1ee9c68d1b6e501ad9c232552e12a6be
Marzdor <marzdor@gmail.com>
2018-04-22 18:05:11 -0700

    got some of it working

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,24 +1,103 @@
-import SmoothScroll from "./modules/SmoothScroll";
+import $ from "jquery";
+const GAMEDATA = {
+  on: false,
+  turns: 0,
+  state: 0,
+  sequence: []
+};
 
-var about = document.querySelectorAll("a[href='#about']");
-var portfolio = document.querySelector("a[href='#portfolio']");
-var contact = document.querySelector("a[href='#contact']");
+$(document).ready(function() {
+  $(".btn_menu-onOff-slider").on("click", powerOnOff);
+  $("#startBtn").on("click", gameLoop);
+});
 
+function gameLoop() {
+  if (GAMEDATA.on) {
+    for (let i = 1; i <= 5; i++) {
+      updateSequence();
+      playSequence();
+      console.log(i);
+    }
+  }
+}
 
-about.forEach(function(element) {
-  element.addEventListener("click", function() {
-    var target = document.querySelector(".section_about");
-    var topPos = target.offsetTop - 40;
-    SmoothScroll(document.body, topPos, 1000);
+function updateSequence() {
+  let btnProperties = [];
+  let newBtn = getRandomBtn();
+
+  btnProperties.push(newBtn);
+
+  let btnColor = newBtn.slice(10, newBtn.length);
+
+  btnProperties.push(btnColor);
+
+  let btnHighlight = "";
+
+  if (btnColor === "green") {
+    btnHighlight = "#08c95e";
+    const greenAudio = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound1.mp3");
+    btnProperties.push(btnHighlight);
+    btnProperties.push(greenAudio);
+  } else if (btnColor === "red") {
+    btnHighlight = "#e01e29";
+    const redAudio = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound2.mp3");
+    btnProperties.push(btnHighlight);
+    btnProperties.push(redAudio);
+  } else if (btnColor === "yellow") {
+    btnHighlight = "#ffe154";
+    const yellowAudio = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound3.mp3");
+    btnProperties.push(btnHighlight);
+    btnProperties.push(yellowAudio);
+  } else if (btnColor === "blue") {
+    btnHighlight = "#0c5db3";
+    const blueAudio = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound4.mp3");
+    btnProperties.push(btnHighlight);
+    btnProperties.push(blueAudio);
+  }
+  GAMEDATA.sequence.push(btnProperties);
+}
+
+function playSequence() {
+  console.log(GAMEDATA.sequence);
+  GAMEDATA.sequence.forEach((seq, index) => {
+    setTimeout(function() {
+      $(seq[0]).css("background", seq[2]);
+      seq[3].play();
+    }, 1000 * index);
   });
-});
-portfolio.addEventListener("click", function() {
-  var target = document.querySelector(".section_portfolio");
-  var topPos = target.offsetTop - 40;
-  SmoothScroll(document.body, topPos, 1000);
-});
-contact.addEventListener("click", function() {
-  var target = document.querySelector(".section_contact");
-  var topPos = target.offsetTop - 40;
-  SmoothScroll(document.body, topPos, 1000);
-});
+}
+
+function getRandomBtn() {
+  let randNum = Math.floor(Math.random() * 4);
+  let btn = ".btn_play-";
+  switch (randNum) {
+    case 0:
+      btn += "green";
+      break;
+    case 1:
+      btn += "red";
+      break;
+    case 2:
+      btn += "yellow";
+      break;
+    case 3:
+      btn += "blue";
+      break;
+  }
+  return btn;
+}
+
+function powerOnOff() {
+  if (!(GAMEDATA.on)) {
+    let countDisplay = GAMEDATA.turns;
+    if (GAMEDATA.turns < 10) {
+      countDisplay = "0" + GAMEDATA.turns;
+    }
+
+    $("#count").text(countDisplay);
+    GAMEDATA.on = true;
+  } else {
+    $("#count").text("--");
+    GAMEDATA.on = false;
+  }
+}
